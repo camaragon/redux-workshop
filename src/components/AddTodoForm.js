b4eddbe0060b40d1ac20da1caa1b37623da95fff
Cameron Aragon <caragon4695@gmail.com>
2021-03-14 19:32:46 -0600

    Add map dispatch to props function

--- src/components/AddTodoForm.js
+++ src/components/AddTodoForm.js
@@ -32,4 +32,8 @@ class AddTodoForm extends Component {
   }
 }
 
-export default AddTodoForm;
+const mapDispatchToProps = dispatch => ({
+  addToDo: text => dispatch(  addToDo(text) )
+});
+
+export default connect(null, mapDispatchToProps)(AddTodoForm);
